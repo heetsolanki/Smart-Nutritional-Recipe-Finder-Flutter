245edf644d397729fb2125e14b30f6142e831deb
heetsolanki <heetsolanki2006@gmail.com>
2025-05-01 21:18:44 +0530

    Added Font Awesome Icons

---
+++ FlavorFind/lib/exports.dart
@@ -0,0 +1,6 @@
+export 'dart:io' show Platform;
+
+export 'package:firebase_core/firebase_core.dart';
+export 'package:flutter/foundation.dart';
+export 'package:flutter/material.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
