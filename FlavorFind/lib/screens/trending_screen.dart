6f8f9655b4a6ee99e77b7e9fb2360199426a140e
heetsolanki <heetsolanki2006@gmail.com>
2025-05-02 20:55:07 +0530

    Created trending_screen.dart

---
+++ FlavorFind/lib/screens/trending_screen.dart
@@ -0,0 +1,14 @@
+import '../exports.dart';
+
+class TrendingScreen extends StatefulWidget {
+  const TrendingScreen({super.key});
+  @override
+  TrendingScreenState createState() => TrendingScreenState();
+}
+
+class TrendingScreenState extends State<TrendingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
