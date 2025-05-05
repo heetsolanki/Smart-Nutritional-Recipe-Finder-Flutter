eed34e9221a9d2c464fa5464251faf9da61e9ef3
heetsolanki <heetsolanki2006@gmail.com>
2025-05-05 09:19:07 +0530

    Updated find_recipes_screen.dart

--- FlavorFind/lib/screens/find_recipes_screen.dart
+++ FlavorFind/lib/screens/find_recipes_screen.dart
@@ -1,5 +1,103 @@
 import '../exports.dart';
 
+class IngredientCategory {
+  final String id;
+  final String name;
+  final String iconPath;
+  final List<String> ingredients;
+  final Map<String, bool> selections;
+
+  IngredientCategory({
+    required this.id,
+    required this.name,
+    required this.iconPath,
+    required this.ingredients,
+    required this.selections,
+  });
+}
+
+class IngredientCategoryCard extends StatelessWidget {
+  final IngredientCategory category;
+  final Function(String, bool) onselectionChanged;
+  const IngredientCategoryCard({
+    super.key,
+    required this.category,
+    required this.onselectionChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 12.0,
+      color: Color.fromRGBO(0, 0, 0, 0.6),
+      child: SizedBox(
+        // height: 300,
+        width: 350,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 18),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset(category.iconPath, height: 35),
+                  SizedBox(width: 10),
+                  Text(
+                    category.name,
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+
+              Wrap(
+                spacing: 8.0,
+                alignment: WrapAlignment.start,
+                runSpacing: 8.0,
+                children:
+                    category.ingredients
+                        .map(
+                          (ingredient) => IntrinsicWidth(
+                            child: CheckboxListTile(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              controlAffinity: ListTileControlAffinity.leading,
+                              tileColor:
+                                  category.selections[ingredient]!
+                                      ? Colors.green[500]
+                                      : Colors.transparent,
+                              activeColor: Color.fromRGBO(108, 88, 76, 1),
+                              checkColor: const Color.fromRGBO(
+                                240,
+                                234,
+                                210,
+                                1,
+                              ),
+                              value: category.selections[ingredient],
+                              onChanged: (value) {
+                                onselectionChanged(ingredient, value!);
+                              },
+                              title: Text(
+                                ingredient,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class FindRecipesScreen extends StatefulWidget {
   const FindRecipesScreen({super.key});
   @override
@@ -7,6 +105,137 @@ class FindRecipesScreen extends StatefulWidget {
 }
 
 class FindRecipesScreenState extends State<FindRecipesScreen> {
+  final List<IngredientCategory> categories = [
+    IngredientCategory(
+      id: 'Dairy',
+      name: 'Dairy',
+      iconPath: 'assets/images/icons/dairy.png',
+      ingredients: ['Milk', 'Paneer', 'Cheese', 'Butter'],
+      selections: {
+        'Milk': false,
+        'Paneer': false,
+        'Cheese': false,
+        'Butter': false,
+      },
+    ),
+    IngredientCategory(
+      id: 'Vegetables',
+      name: 'Vegetables',
+      iconPath: 'assets/images/icons/vegetables.png',
+      ingredients: ['Carrot', 'Potato', 'Tomato', 'Onion'],
+      selections: {
+        'Carrot': false,
+        'Potato': false,
+        'Tomato': false,
+        'Onion': false,
+      },
+    ),
+    IngredientCategory(
+      id: 'Fruits',
+      name: 'Fruits',
+      iconPath: 'assets/images/icons/fruits.png',
+      ingredients: ['Apple', 'Banana', 'Grapes', 'Mango'],
+      selections: {
+        'Apple': false,
+        'Banana': false,
+        'Grapes': false,
+        'Mango': false,
+      },
+    ),
+    IngredientCategory(
+      id: 'Nuts & Seeds',
+      name: 'Nuts & Seeds',
+      iconPath: 'assets/images/icons/nuts.png',
+      ingredients: ['Almond', 'Cashew', 'Flax Seeds', 'Chia Seeds'],
+      selections: {
+        'Almond': false,
+        'Cashew': false,
+        'Flax Seeds': false,
+        'Chia Seeds': false,
+      },
+    ),
+    IngredientCategory(
+      id: 'Grains & Cereals',
+      name: 'Grains & Cereals',
+      iconPath: 'assets/images/icons/grains.png',
+      ingredients: ['Wheat', 'Barley', 'Oats', 'Rice'],
+      selections: {
+        'Wheat': false,
+        'Barley': false,
+        'Oats': false,
+        'Rice': false,
+      },
+    ),
+    IngredientCategory(
+      id: 'Legumes',
+      name: 'Legumes',
+      iconPath: 'assets/images/icons/legumes.png',
+      ingredients: ['Lentils', 'Chickpeas', 'Peas', 'Beans'],
+      selections: {
+        'Lentils': false,
+        'Chickpeas': false,
+        'Peas': false,
+        'Beans': false,
+      },
+    ),
+    IngredientCategory(
+      id: 'Oils',
+      name: 'Oils',
+      iconPath: 'assets/images/icons/oils.png',
+      ingredients: [
+        'Olive Oil',
+        'Peanut Oil',
+        'Corn Oil',
+        'Sesame Oil',
+        'Canola Oil',
+      ],
+      selections: {
+        'Olive Oil': false,
+        'Peanut Oil': false,
+        'Corn Oil': false,
+        'Sesame Oil': false,
+        'Canola Oil': false,
+      },
+    ),
+  ];
+
+  void _handleSelections(
+    IngredientCategory category,
+    String ingredient,
+    bool value,
+  ) {
+    setState(() {
+      category.selections[ingredient] = value;
+    });
+  }
+
+  void _saveAndFindRecipes() {
+    final selectedIngredients = <String>[];
+    for (var category in categories) {
+      category.selections.forEach((ingredient, isSelected) {
+        if (isSelected) {
+          selectedIngredients.add(ingredient);
+        }
+      });
+    }
+
+    if (selectedIngredients.isNotEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Selected Ingredients: $selectedIngredients'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please select at least 2 ingredients'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,18 +268,38 @@ class FindRecipesScreenState extends State<FindRecipesScreen> {
               duration: Duration(milliseconds: 250),
               children: [
                 Text(
-                  'Find Recipes',
+                  'Select ingredients: ',
                   style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 25,
                     fontWeight: FontWeight.w600,
                     color: Colors.white,
                   ),
-                  textAlign: TextAlign.center,
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(height: 30),
+                ...categories.map(
+                  (category) => IngredientCategoryCard(
+                    category: category,
+                    onselectionChanged:
+                        (ingredient, value) =>
+                            _handleSelections(category, ingredient, value),
+                  ),
                 ),
               ],
             ),
           ),
         ],
+      ),
+      floatingActionButton: SizedBox(
+        height: 56.0,
+        width: 118.0,
+        child: FloatingActionButton(
+          onPressed: _saveAndFindRecipes,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          backgroundColor: Color.fromRGBO(108, 88, 76, 1),
+          foregroundColor: Color.fromRGBO(240, 234, 210, 1),
+          child: Text('Find Recipes', style: TextStyle(fontSize: 18)),
+        ),
       ),
     );
   }
