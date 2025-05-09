8322a9414e066c5786b244a83420259a9cb47f6c
heetsolanki <heetsolanki2006@gmail.com>
2025-05-09 11:37:32 +0530

    Added new exports

--- FlavorFind/lib/exports.dart
+++ FlavorFind/lib/exports.dart
@@ -1,12 +1,13 @@
 export 'dart:io' show Platform;
 
-// export 'package:cloud_firestore/cloud_firestore.dart';
 export 'package:firebase_core/firebase_core.dart';
 export 'package:flutter/foundation.dart';
 export 'package:flutter/material.dart';
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
 export 'package:smooth_list_view/smooth_list_view.dart';
 
+export 'recipe_card.dart';
+export 'recipe_screens/potato_cheese_oliveoil.dart';
 export 'screens/about_screen.dart';
 export 'screens/find_recipes_screen.dart';
 export 'screens/home_screen.dart';
