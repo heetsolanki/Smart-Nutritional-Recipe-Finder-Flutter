fb805b469cbb5f022056f8554450328c9faafde1
heetsolanki <heetsolanki2006@gmail.com>
2025-05-12 10:27:05 +0530

    Updated exports.dart

--- FlavorFind/lib/exports.dart
+++ FlavorFind/lib/exports.dart
@@ -1,8 +1,10 @@
 export 'dart:io' show Platform;
 
+export 'package:bulleted_list/bulleted_list.dart';
 export 'package:firebase_core/firebase_core.dart';
 export 'package:flutter/foundation.dart';
 export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
 export 'package:smooth_list_view/smooth_list_view.dart';
 
@@ -14,4 +16,6 @@ export 'recipes.dart';
 export 'screens/about_screen.dart';
 export 'screens/find_recipes_screen.dart';
 export 'screens/home_screen.dart';
+export 'screens/recipe_screen.dart';
 export 'screens/trending_screen.dart';
+export 'set_favorite.dart';
