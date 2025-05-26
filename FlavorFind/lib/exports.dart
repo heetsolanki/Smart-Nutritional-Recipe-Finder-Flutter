319e13b65acc64df650a1b428a9c417bebe36da8
heetsolanki <heetsolanki2006@gmail.com>
2025-05-26 16:14:37 +0530

    New exports added

--- FlavorFind/lib/exports.dart
+++ FlavorFind/lib/exports.dart
@@ -7,15 +7,17 @@ export 'package:firebase_core/firebase_core.dart';
 export 'package:flutter/material.dart';
 export 'package:flutter/services.dart';
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+export "package:simple_icons/simple_icons.dart";
 export 'package:smooth_list_view/smooth_list_view.dart';
+export 'package:url_launcher/url_launcher.dart';
 
 // Screens
-export 'recipe_card.dart';
+export 'matched_recipes_screen.dart';
 export 'recipes.dart';
 export 'screens/about_screen.dart';
+export 'screens/detailed_recipe_screen.dart';
 export 'screens/favorite_recipes_screen.dart';
 export 'screens/find_recipes_screen.dart';
 export 'screens/home_screen.dart';
-export 'screens/recipe_screen.dart';
 export 'screens/trending_screen.dart';
 export 'set_favorite.dart';
